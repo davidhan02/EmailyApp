3cc5cf79241671b1f8cd6ad4d25a35145acff146
David Han <davidhan.5.25@gmail.com>
2019-04-16 18:18:16 -0700

    refactor client authReducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,8 +1,9 @@
-const initialState = {};
+import { FETCH_USER } from '../actions/types';
 
-export default function(state = initialState, action) {
-  console.log(action);
+export default function(state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }

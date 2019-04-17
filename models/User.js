65a3f78e47bab73a19831326c1b398dafb07751a
David Han <davidhan.5.25@gmail.com>
2019-04-16 22:34:06 -0700

    Start requiring authentication for /api/stripe billing route

--- models/User.js
+++ models/User.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  googleId: String
+  googleId: {
+    type: String
+  },
+  credits: {
+    type: Number,
+    default: 0
+  }
 });
 
 module.exports = mongoose.model('users', userSchema);

7c0a9feafdebf3abcdac75b48176c3e2aa95c14c
David Han <davidhan.5.25@gmail.com>
2019-04-16 18:25:54 -0700

    Add access state in Header component to see if logged in or not

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return 'Still deciding';
+      case false:
+        return 'Logged out';
+      default:
+        return 'Logged in';
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">Emaily</a>
-          <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = ({ auth }) => ({ auth });
+
+export default connect(mapStateToProps)(Header);

e0698ca9184aeba18dae23707972e00feab62f8d
David Han <davidhan.5.25@gmail.com>
2019-04-17 20:58:10 -0700

    Set up SubDoc collection

--- models/Survey.js
+++ models/Survey.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
   title: {
@@ -15,7 +16,7 @@ const surveySchema = new Schema({
     required: true
   },
   recipients: {
-    type: [String],
+    type: [RecipientSchema],
     required: true
   },
   yes: {

394034e65805e47267dada4371eb89b2f3291d80
David Han <davidhan.5.25@gmail.com>
2019-04-18 18:05:58 -0700

    Added redux-form and set up SurveyNew component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,7 @@ import * as actions from '../actions';
 import Header from './common/Header';
 import Landing from './Landing';
 import Dashboard from './Dashboard';
-const SurveyNew = () => <h2>SurveyNew</h2>;
+import SurveyNew from './surveys/SurveyNew';
 
 class App extends Component {
   componentDidMount() {
---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SurveyNew extends Component {
+  render() {
+    return (
+      <div>
+        <h1>SurveyNew!</h1>
+      </div>
+    );
+  }
+}
+
+export default SurveyNew;

9df64935f8b288c7c7be5efcb421f1a2a33ff8c0
David Han <davidhan.5.25@gmail.com>
2019-04-18 18:34:27 -0700

    Refactor provider store into separate js file in client src, add redux_dev_tools

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,12 @@ import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
 
+import store from './store';
 import App from './components/App';
-import reducers from './reducers';
 // Dev only axios helpers
 import axios from 'axios';
 window.axios = axios;
-
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>

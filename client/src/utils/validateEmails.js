d7ea3fad4a4c6cadb069893f4fbaf127f921de3e
David Han <davidhan.5.25@gmail.com>
2019-04-18 21:49:55 -0700

    Add more validation to email

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -1,15 +1,15 @@
+// eslint-disable-next-line
 const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export default emails => {
   const invalidEmails = emails
+    .replace(/\s/g, '') // Remove all white space characters
+    .replace(/,,+/g, ',') // Remove any surplus commas from anywhere in the string
+    .replace(/(^,|,$)/g, '') // Remove a comma from the start or end of the string
     .split(',')
-    .map(email => email.trim())
     .filter(email => regex.test(email) === false);
 
   if (invalidEmails.length) {
-    if (invalidEmails.includes('')) {
-      return 'There is a trailing comma';
-    }
     return `These emails are invalid: ${invalidEmails.join(', ')}`;
   }
 

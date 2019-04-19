1a0ca7ab6b11bfb246459ff931180a85fbe4651a
David Han <davidhan.5.25@gmail.com>
2019-04-19 15:02:37 -0700

    Finalizing refactor of SurveyFormReview with import of formFields

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields';
 
 const SurveyFormReview = ({ onCancel, formValues }) => {
-  console.log(formValues);
+  const reviewFields = formFields.map(({ label, name }) => (
+    <div key={name}>
+      <label>{label}</label>
+      <div>{formValues[name]}</div>
+    </div>
+  ));
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
+      {reviewFields}
       <button className="yellow darken-3 btn-flat" onClick={onCancel}>
         Back
       </button>
